f54827162b47eb0b5a8ac50d9d287dca26a745fd
Aro10000 <aronmondezie100@gmail.com>
2024-07-03 15:33:24 -0400

    STEP 25D: Understanding Method Decorators and their Usage: (..@api.constrains('field')..)

--- custom_odoo_cmms/real_estate_ads/models/property_offer.py
+++ custom_odoo_cmms/real_estate_ads/models/property_offer.py
@@ -1,7 +1,10 @@
 from odoo import fields, models, api
 from datetime import timedelta
-
+from odoo.exceptions import ValidationError
 #        Forth MODEL
+
+
+
 class PropertOffer(models.Model):
     _name = 'estate.property.offer'
     _description = 'Estate Properties offer'
@@ -21,9 +24,9 @@ class PropertOffer(models.Model):
     #... creation_date = fields.Date(string='Create Date', default=_set_create_date)
     creation_date = fields.Date(string='Create Date')
 
-    # Step 22B: Understanding Computed Fields and On-change ORM Decorator
+    # Step 22-B: Understanding Computed Fields and On-change ORM Decorator
     # This is Understanding Computed Fields
-    @api.depends('validity', 'creation_date')# STEP 25D: Understanding Method Decorators and their Usage: (..@api.depends_context()... optional)
+    @api.depends('validity', 'creation_date')# STEP 25-D: Understanding Method Decorators and their Usage: (..@api.depends_context()... optional)
     def _compute_deadline(self):
         for rec in self:
             if rec.creation_date and rec.validity:#                                        Import "from datetime import timedelta " (insert ABOVE) to computer date fields
@@ -39,15 +42,22 @@ class PropertOffer(models.Model):
         else:
             rec.validity = False
 
-    # STEP 25A: Understanding Method Decorators and their Usage
+    # STEP 25-A: Understanding Method Decorators and their Usage
     #...@api.autovacuum  # this Decorator deletes "refused" data everyday  ( settings/Schedule Action)
     #...def _cleans_offers(self):
     #...    self.search([('status', '=', 'refused')]).unlink()  # unlink means delete
 
-    # STEP 25C: Understanding Method Decorators and their Usage: (@api.model_create_multi)
+    # STEP 25-C: Understanding Method Decorators and their Usage: (@api.model_create_multi)
     @api.model_create_multi
     def create(self, vals):
         for rec in vals:
             if not rec.get('creation_date'):
                 rec['creation_date'] = fields.Date.today()
         return super(PropertOffer, self).create(vals)
+
+    # STEP 25-C: Understanding Method Decorators and their Usage: (..@api.constrains('field')..)
+    @api.constrains('validity')
+    def _check_validity(self):
+        for rec in self:
+            if rec.deadline <= rec.creation_date:
+                raise ValidationError("Deadline cannot be before creation date")
