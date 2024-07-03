38b3e547ce372a54339f2eb4a2aed41ae9c33b15
Aro10000 <aronmondezie100@gmail.com>
2024-07-03 07:37:46 -0400

    STEP 25B: Understanding Method Decorators and their Usage: (@api.model Decorator)

--- custom_odoo_cmms/real_estate_ads/models/property_offer.py
+++ custom_odoo_cmms/real_estate_ads/models/property_offer.py
@@ -13,16 +13,21 @@ class PropertOffer(models.Model):
     property_id = fields.Many2one('estate.property', string='Property')
     validity = fields.Integer(string='Validity (days)')
     deadline = fields.Date(string='Deadline', compute='_compute_deadline', inverse='_inverse_deadline')# Always use Compute And Inverse Attributes: for Compute And Inverse Fields
-    creation_date = fields.Date(string='Create Date')
+
+    # STEP 25B: Understanding Method Decorators and their Usage: (@api.model Decorator)
+    @api.model
+    def _set_create_date(self):
+        return fields.Date.today()
+    creation_date = fields.Date(string='Create Date', default=_set_create_date)
 
     # Step 22B: Understanding Computed Fields and On-change ORM Decorator
     # This is Understanding Computed Fields
     @api.depends('validity', 'creation_date')
     def _compute_deadline(self):
         for rec in self:
-            if rec.creation_date and rec.validity:# Import "from datetime import timedelta " (insert ABOVE) to computer date fields
-                rec.deadline = rec.creation_date + timedelta(days=rec.validity)# You will always getan error trying to directly compute 2 differebt types of fields....
-            else:#...but use "else" to control computing and stop "errors"
+            if rec.creation_date and rec.validity:#                                     Import "from datetime import timedelta " (insert ABOVE) to computer date fields
+                rec.deadline = rec.creation_date + timedelta(days=rec.validity)#        You will always getan error trying to directly compute 2 differebt types of fields....
+            else:#                                                                      but use "else" to control computing and stop "errors"
                 rec.deadline = False
 
     def _inverse_deadline(self):
